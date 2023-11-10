96b4cf71ebe11ddc34ab417898b47da13385bf62
Vincent <Vincent.s.kleis@gmail.com>
2023-11-10 11:27:33 +0100

    worked on stacking cards

--- Hobby/solitaire/solitaire.py
+++ Hobby/solitaire/solitaire.py
@@ -17,10 +17,10 @@ HIDDEN_CARD = [
 "└─────────┘"]
 
 name_to_symbol = {
-        'Spades':   '\u2660',
-        'Diamonds': '\u2662',
-        'Hearts':   '\u2661',
-        'Clubs':    '\u2663',
+        'spades':   '\u2660',
+        'diamonds': '\u2662',
+        'hearts':   '\u2661',
+        'clubs':    '\u2663',
     }
 
 ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
@@ -49,7 +49,7 @@ class solitaire():
         self.ordered_stack = [EMPTY_SPOT for i in range(4)]
 
 
-    def get_ascii(self, suit, rank)-> list:
+    def get_ascii(self, rank, suit)-> list:
         card = [
         "┌─────────┐",
         "│{}     {}│".format(f'{rank:<2}', f'{suit:<2}'),
@@ -137,6 +137,7 @@ class solitaire():
 
     def draw(self):
         if len(self.un_drawn_cards) == 0:
+            # resets the un drawn stack
             self.un_drawn_cards = self.drawn_cards
             self.drawn_cards = []
         else:
@@ -144,42 +145,66 @@ class solitaire():
             self.drawn_cards.append(card)
 
 
-    def stack(self, card1:str, card2:str):
-        card1 = card1.split(',')
-        card2 = card2.split(',')
+    def legal_move(self, card1, card2)->str:
+        """test if card1 can stack on card2
 
-        # test if it is theoreticaly possible
+        Args:
+            card1 (tuple): first card
+            card2 (tuple): second card
+
+        Returns:
+            str: descriptive error message
+        """
         match card1[0]:
-            case 'A':
-                card1 = [1, card1[1]]
-            case 'J':
-                card1 = [11, card1[1]]
-            case 'Q':
-                card1 = [12, card1[1]]
-            case 'K':
-                card1 = [13, card1[1]]
+            case 'a':
+                test1 = [1, card1[1]]
+            case 'j':
+                test1 = [11, card1[1]]
+            case 'q':
+                test1 = [12, card1[1]]
+            case 'k':
+                test1 = [13, card1[1]]
             case _:
-                card1 = [int(card1[0]), card1[1]]
+                test1 = [int(card1[0]), card1[1]]
 
         match card2[0]:
-            case 'A':
-                card2 = [1, card2[1]]
-            case 'J':
-                card2 = [11, card2[1]]
-            case 'Q':
-                card2 = [12, card2[1]]
-            case 'K':
-                card2 = [13, card2[1]]
+            case 'a':
+                test2 = [1, card2[1]]
+            case 'j':
+                test2 = [11, card2[1]]
+            case 'q':
+                test2 = [12, card2[1]]
+            case 'k':
+                test2 = [13, card2[1]]
             case _:
-                card2 = [int(card2[0]), card2[1]]
-        
+                test2 = [int(card2[0]), card2[1]]
 
-        if card1[1] == '♠' and card2[1] == '♣' or card1[1] == '♣' and card2[1] == '♠' or\
-            card1[1] == '♦' and card2[1] == '♥' or card1[1] == '♥' and card2[1] == '♦' :
+        if card1[1] == '\u2660' and card2[1] == '\u2663' or card1[1] == '\u2663' and card2[1] == '\u2660' or\
+            card1[1] == '\u2662' and card2[1] == '\u2661' or card1[1] == '\u2661' and card2[1] == '\u2662' :
             return f'{card1[1]}, {card2[1]}these suits do not stack'
     
-        if int(card1[0]) != int(card2[0])-1:
+        if int(test1[0]) != int(test2[0])-1:
             return f'{card1[0]}, {card2[9]}the rank of card1 is not one smaller than the rank of card2'
+        
+        return None
+    
+    def on_table(card)->tuple:
+        
+        
+
+    def stack(self, card1:str, card2:str):
+        card1 = card1.split(',')
+        card1 = [card1[0].strip().lower(), name_to_symbol[card1[1].strip().lower()]]
+
+        card2 = card2.split(',')
+        card2 = [card2[0].strip().lower(), name_to_symbol[card2[1].strip().lower()]]
+
+        # test if it is theoreticaly possible
+        responce = self.legal_move(card1, card2)
+        if responce != None:
+            print(responce)
+
+        index1 = None
         
         # find the cards and remove the first card from it's position if it is found
         for i in range(len(self.fliped_cards)):
@@ -195,7 +220,6 @@ class solitaire():
                     index1 = (i, indexx)
                     self.fliped_cards[index1[0]].pop(index1[1])
 
-
             if card2[0] in stack_ranks and card2[1] in stack_suits:
                 indexx = stack_ranks.index(card2[0])
                 cardx = stack[indexx]
@@ -203,13 +227,27 @@ class solitaire():
                 if cardx[0] == card2[0] and cardx[1] == card2[1]:
                     index2 = (i, indexx)
 
-        if self.drawn_cards[0][0] == card1[0][0] and self.drawn_cards[0][1] == card1[0][1]:
-            index1 = 
+        if self.drawn_cards != []:
+            if self.drawn_cards[0][0] == card1[0][0] and self.drawn_cards[0][1] == card1[0][1]:
+                print(self.drawn_cards[0])
+                index1 = 0
+                self.drawn_cards.pop(0)    
 
         # move the cards
-        index2 = self.fliped_cards.index(card2)
-        
-        self.fliped_cards[index2[0]].append((str(card1[0]), card1[1]))
+        index2 = None
+        for i in range(len(self.fliped_cards)):
+            for j in range(len(self.fliped_cards[i])):
+                if self.fliped_cards[i][j][0] == card2[0] and self.fliped_cards[i][j][1] == card2[1]:
+                    index2 = (i, j)
+
+        if index1 != None:
+            self.fliped_cards[index2[0]].append(str(card1[0]), card1[1])
+        if index1 == None and index2 != None:
+            return f'{card1} is not a card on the board'
+        if index1 != None and index2 == None:
+            return f'{card2} is not a card on the board'
+        if index1 == None and index2 == None:
+            return f'{card1} ad {card2} are not cards on the board'
 
         
 
@@ -218,9 +256,11 @@ deck = solitaire()
 while True:
     cls()
     deck.show_on_tab()
-    responce = input('cards should be written as "rank, suit" examples "ace, spades", "1, clubs", "king, hearts", "jack, diamonds".\
+    responce = input(f'cards should be written as "s rank1, suit1 - rank2, suit2" examples "s A, spades - K, hearts", \
+                     \n"s Q, clubs - J, diamonds", "s 3, clubs - 4, hearts"\
                      \nempty spots are formated as "t" for table and "o" for ordered spot and there are 4 "o" spots and 7 "t" spots\
                      \nso t7 for 7th spot on the table and o3 for 3rd spot on the orderes spots\n\
+                     \nhearts={name_to_symbol["hearts"]}, spades={name_to_symbol["spades"]}, clubs={name_to_symbol["clubs"]}, diamonds={name_to_symbol["diamonds"]}\n\
                      \ns + "card1 - card2" = stack card 1 on card 2\
                      \nm + "card - spot"= move card to spot\
                      \nd = draw card from pile\
@@ -231,9 +271,12 @@ while True:
     if responce.lower() == 'd':
         deck.draw()
     if responce.lower()[0] == 's':
+        responce = responce.lstrip('s')
         card1, card2 = responce.lower().split('-')[0], responce.lower().split('-')[1]
-        responce = deck.stack(card1, card2)
-        cls()
-        print(responce)
-        sleep(2)
+        error = None
+        error = deck.stack(card1, card2)
+        if error != None:
+            cls()
+            print(error)
+            sleep(3)
 
